c40e9adad9840a50e4f5965d08917447dcea7fd1
Chu-ching Huang <cchuang2009@gmail.com>
2016-07-11 09:02:49 +0800

    Source of 2016 portable Python Lectures

---
+++ portable.bat
@@ -0,0 +1,15 @@
+﻿@echo off
+cls
+set DISK=none
+set FLAG=AnacondaWin
+
+echo Wait please, searching for current drive letter.
+for %%d in ( C D E F G H I J K L M N O P Q R S T U V W X Y Z ) do if exist %%d:\%FLAG% set DISK=%%d
+
+set PYTHON_PATH=%DISK%:\AnacondaWin\
+
+set PATH=%PYTHON_PATH%Scripts;%PYTHON_PATH%Tools\scripts;%PATH%;
+
+
+%PYTHON_PATH%Scripts\jupyter-notebook.exe
+REM start "F:\AnacondaWin\Scripts\jupyter-notebook.exe"
